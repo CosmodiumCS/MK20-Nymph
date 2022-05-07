727b172507c21ebc484329ecb9df09386b3768fa
Soulsender <soulsender@tutanota.com>
2022-05-07 15:44:26 -0700

    fixed more paths

--- source/desktop.sh
+++ source/desktop.sh
@@ -7,15 +7,13 @@ blue="\e[0;94m"
 bold="\e[1m"
 reset="\e[0m"
 
-cd $HOME
-
 apt-get install -y kde-plasma-desktop
 apt-get install -y feh
 apt-get install -y git
 
 # install kde theme
 echo -e "${blue}Installing KDE theme..."
-cd $HOME
+cd ~
 git clone https://github.com/yeyushengfan258/LyraS-kde
 cd LyraS-kde
 cat AUTHORS
@@ -30,9 +28,9 @@ cp -r -f ~/nymph/assets/LyraS-dark ~/.local/share/plasma/desktoptheme
 cp -r -f ~/nymph/assets/color-schemes ~/.local/share
 
 # change wallpaper
-cp $HOME/nymph/assets/wallpapers $HOME/Pictures
-feh -bg --scale $HOME/Pictures/wallpapers/saints-name.png
-sed -i "$ a feh -bg --scale $HOME/Pictures/saints-name.png" .xinitrc
+cp ~/nymph/assets/wallpapers ~/Pictures
+feh -bg --scale ~/Pictures/wallpapers/saints-name.png
+sed -i "$ a feh -bg --scale ~/Pictures/saints-name.png" .xinitrc
 
 # finish installation
 echo -e "${green}Desktop installation successful${reset}"
