22b02696697205cf02d82ba1a5bd12cbbd53cd88
Soulsender <soulsender@tutanota.com>
2022-05-07 16:49:01 -0700

    commit

--- source/desktop.sh
+++ source/desktop.sh
@@ -13,7 +13,7 @@ apt-get install -y git
 
 # install kde theme
 echo -e "${blue}Installing KDE theme..."
-cd ~
+cd /home/$SUDO_USER
 git clone https://github.com/yeyushengfan258/LyraS-kde
 cd LyraS-kde
 cat AUTHORS
@@ -23,14 +23,14 @@ chmod +x install.sh
 # copy kde configuration to home directory
 echo -e "${blue}Copying configuration files..."
 sleep 2
-cp -r -f /home/$SUDO_USER/Nymph/nymph/assets/.kde4 ~
-cp -r -f /home/$SUDO_USER/Nymph/assets/LyraS-dark ~/.local/share/plasma/desktoptheme
-cp -r -f /home/$SUDO_USER/Nymph/assets/color-schemes ~/.local/share
+cp -r -f /home/$SUDO_USER/Nymph/assets/.kde4 /home/$SUDO_USER
+cp -r -f /home/$SUDO_USER/Nymph/assets/LyraS-dark /home/$SUDO_USER/.local/share/plasma/desktoptheme
+cp -r -f /home/$SUDO_USER/Nymph/assets/color-schemes /home/$SUDO_USER/.local/share
 
 # change wallpaper
-cp /home/$SUDO_USER/Nymph/assets/wallpapers /home/$SUDO_USER/Pictures
+cp -r -f /home/$SUDO_USER/Nymph/assets/wallpapers /home/$SUDO_USER/Pictures
 feh -bg --scale /home/$SUDO_USER/Pictures/wallpapers/saints-name.png
-sed -i "$ a feh -bg --scale ~/Pictures/saints-name.png" .xinitrc
+sed -i "$ a feh -bg --scale $SUDO_USER/Pictures/saints-name.png" .xinitrc
 
 # finish installation
 echo -e "${green}Desktop installation successful${reset}"
