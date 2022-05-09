50d9a2492871a0ef4f53980fb51ff239fa54a281
Soulsender <soulsender@tutanota.com>
2022-05-09 13:08:30 -0700

    copied files

--- source/desktop.sh
+++ source/desktop.sh
@@ -24,11 +24,18 @@ chmod +x install.sh
 echo -e "${blue}Copying configuration files..."
 sleep 2
 cp -r -f /home/$SUDO_USER/Nymph/assets/.kde4 /home/$SUDO_USER
-cp -r -f /home/$SUDO_USER/Nymph/assets/LyraS-dark /home/$SUDO_USER/.local/share/plasma/desktoptheme
+mv -f /home/$SUDO_USER/.kde4 /home/$SUDO_USER/.kde
 cp -r -f /home/$SUDO_USER/Nymph/assets/color-schemes /home/$SUDO_USER/.local/share
+mkdir /home/$SUDO_USER/.local/share/icons
+cp -r -f /home/$SUDO_USER/Nymph/assets/candy-icons /home/$SUDO_USER/.local/share/icons
+cp -r -f /home/$SUDO_USER/Nymph/assets/kdedefaults /home/$SUDO_USER/.config/
+cp -r -f /home/$SUDO_USER/Nymph/assets/plasmarc
+cp -r -f /home/$SUDO_USER/Nymph/assets/candy-icons /home/$SUDO_USER/
+cp -r -f /home/$SUDO_USER/Nymph/assets/plasma /home/$SUDO_USER/.local/share
 
 # change wallpaper
 cp -r -f /home/$SUDO_USER/Nymph/assets/wallpapers /home/$SUDO_USER/Pictures
+cp -r -f /home/$SUDO_USER/Nymph/assets/wallpapers /usr/share/backgrounds
 feh -bg --scale /home/$SUDO_USER/Pictures/wallpapers/saints-name.png
 sed -i "$ a feh -bg --scale $SUDO_USER/Pictures/saints-name.png" .xinitrc
 
