41fd833120b99fe37306576d8c9a97e40b7717f1
HL0 <130487372+hall0wedccs@users.noreply.github.com>
2023-05-05 20:53:49 +0100

    Unnecessary file
    makescript.sh checks if its being ran for the first time and if so it creates set_desktop_variables.sh, this file isn't necessary and only exists cause i was testing makescript.sh on my system, this file will automatically be made for YOUR system using makescript.sh, don't delete makescript.sh though.

--- assets/undercover/set_desktop_variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-gsettings set org.cinnamon.theme name "'Mint-Y-Dark-Red'"
-gsettings set org.cinnamon panels-enabled ['1:0:top']
-gsettings set org.cinnamon.desktop.interface icon-theme "'BeautyLine'"
-gsettings set org.cinnamon.desktop.wm.preferences theme "'Mint-Y-Dark-Red'"
-gsettings set org.cinnamon.desktop.interface gtk-theme "'Mint-Y-Dark-Red'"
-gsettings set org.cinnamon.desktop.background picture-uri "'file:///home/hall0w/Downloads/dark.jpg'"
