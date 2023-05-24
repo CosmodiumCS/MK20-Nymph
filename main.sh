e1be72918b49f010dde8dfe666315765af3fa08c
youcantosintme <you-cant@OSINT.me>
2023-05-24 12:41:57 +0100

    added shit

--- main.sh
+++ main.sh
@@ -1,5 +1,34 @@
 #!/bin/bash
 
+# Define the name of the script that will be generated
+SCRIPT_NAME="preferences-DO_NOT_DELETE.sh"
+
+# Check if the script already exists
+if [ ! -f $SCRIPT_NAME ]; then
+  
+  # Get the current desktop variables
+  THEME_NAME=$(gsettings get org.cinnamon.theme name)
+  PANELS_ENABLED=$(gsettings get org.cinnamon panels-enabled)
+  ICON_THEME=$(gsettings get org.cinnamon.desktop.interface icon-theme)
+  WM_THEME=$(gsettings get org.cinnamon.desktop.wm.preferences theme)
+  GTK_THEME=$(gsettings get org.cinnamon.desktop.interface gtk-theme)
+  BACKGROUND=$(gsettings get org.cinnamon.desktop.background picture-uri)
+  
+  # Create the script that will set the desktop variables
+  echo "#!/bin/bash" > $SCRIPT_NAME
+  echo "gsettings set org.cinnamon.theme name $THEME_NAME" >> $SCRIPT_NAME
+  echo "gsettings set org.cinnamon panels-enabled \"$PANELS_ENABLED"\" >> $SCRIPT_NAME
+  echo "gsettings set org.cinnamon.desktop.interface icon-theme $ICON_THEME" >> $SCRIPT_NAME
+  echo "gsettings set org.cinnamon.desktop.wm.preferences theme $WM_THEME" >> $SCRIPT_NAME
+  echo "gsettings set org.cinnamon.desktop.interface gtk-theme $GTK_THEME" >> $SCRIPT_NAME
+  echo "gsettings set org.cinnamon.desktop.background picture-uri $BACKGROUND" >> $SCRIPT_NAME
+  
+  # Make the script executable
+  chmod +x $SCRIPT_NAME
+else
+  echo "Previous Config Detected"
+fi
+
 # declare variables
 red="\e[0;91m"
 green="\e[0;92m"
@@ -15,134 +44,84 @@ INSTALL=false
 
 # get client variables
 export HOME_DIR=/home/$SUDO_USER
-export WORKING_DIR=`pwd`
+export WORKING_DIR=$(dirname "$(readlink -f "$0")") # The directory of the script itself
 export TOOL_DIR=$WORKING_DIR/src/tools.sh
 export DESKTOP_DIR=$WORKING_DIR/src/desktop.sh
 
-echo $HOME_DIR
-echo $WORKING_DIR
-echo $TOOL_DIR
-echo $DESKTOP_DIR
-
-
-# check for arguments
-if [[ $# -eq 0 ]] ; then
-    echo -e "${red}No arguments specified. Use -h to see a list of possible arguments.${reset}"
-    exit 0
-fi
-
-# arguments
-while [ -n "$1" ]
-do
-case "$1" in
---help)
-  cat assets/help.txt
-  exit 0
-;;
---desktop) 
-  DESKTOP=true
-;;
---tools) 
-  TOOLS=true
-;;
-*) 
-  echo -e "${red}$1 is not an option. See -h for a list of arguments. ${reset}" 
-  exit 0
-;;
-esac
-shift
-done
-
-# check if run with sudo
-if [ "$EUID" -ne 0 ]
-    then echo -e "${red}Please run as root${reset}"
-    exit 0
-fi
 
 # FORCE THEM TO SEE THE ASCII ART BECAUSE ITS COOL
 echo -e ''${blue}${bold}'
- ..                          
-  ...                        
-  ....                      
-    .....                    
-    ........                 
-    ..........         __                           _                      
-    ............    /\ \ \ _   _  _ __ ___   _ __  | |__                 
-     ...........   /  \/ /| | | || `_ ` _ \ | `_ \ | `_ \      
-      ..........  / /\  / | |_| || | | | | || |_) || | | |              
-        ........  \_\ \/   \__, ||_| |_| |_|| .__/ |_| |_|                
-            ....      '$version' |___/            |_|          
-           .. ..                             
-             ..             '${reset}${bold}'By Soulsender'${reset}${blue}'
+                             __                           _
+                          /\ \ \ _   _  _ __ ___   _ __  | |__
+                         /  \/ /| | | || `_ ` _ \ | `_ \ | `_ \
+                        / /\  / | |_| || | | | | || |_) || | | |
+                        \_\ \/   \__, ||_| |_| |_|| .__/ |_| |_|
+                            '$version' |___/            |_|
+           
+          
+                            '${reset}${bold}'By Soulsender & Hall0wed'${reset}${blue}'
                             'soulsender.me'
                       '${reset}''
-
 
 echo -e "Detecting System and Preferences..."
 sleep 2
 echo -e ""
 
-# INSTALL components = true
-if [ "$TOOLS" = true ]
-  then
+# Command line options
+function tools() {
   echo -e "${green}[!] Found Tools${reset}"
-fi
+}
 
-if [ "$DESKTOP" = true ]
-  then
+function desktop() {
   echo -e "${green}[!] Found Desktop${reset}"
-fi
+}
 
-# INSTALL components = false
-if [ "$TOOLS" = false ]
-  then 
-  echo -e "${red}[!] Skipping Tools Installation${reset}"
-fi
+function undercover() {
+  echo -e "${green}[!] Undercover${reset}"
 
-if [ "$DESKTOP" = false ]
-  then 
-  echo -e "${red}[!] Skipping Desktop Installation${reset}"
-fi
+  gsettings set org.cinnamon.theme name 'Windows-10-Dark-3.2.1-dark'
+  gsettings set org.cinnamon panels-enabled "['1:0:bottom']" 
+  gsettings set org.cinnamon.desktop.interface icon-theme 'Windows-10-Icons'
+  gsettings set org.cinnamon.desktop.wm.preferences theme 'Windows-10-Dark-3.2.1-dark'
+  gsettings set org.cinnamon.desktop.interface gtk-theme "Windows-10-Dark-3.2.1-dark"
+  gsettings set org.cinnamon.desktop.background picture-uri "file://$PWD/assets/undercover/windows/windows10wallpaper.jpg"
+  sed -i 's/favicon_dark.svg/windows10logo.png/g' ~/.config/cinnamon/spices/menu@cinnamon.org/0.json
 
-# continue prompt
-while true
-do
-  read -r -p "Is the selection correct, and would you like to begin the installation? [Y/n]" input
-  case $input in
-    [yY][eE][sS]|[yY])
-      echo -e "${green}Beginning Installation...${reset}"
-      INSTALL=true
-      break
+}
+
+function reverse_undercover() {
+  echo -e "${green}[!] Reverse Undercover${reset}"
+  ./preferences-DO_NOT_DELETE.sh
+}
+
+while true; do
+  echo -e "Please select an option:"
+  echo -e "1. Tools"
+  echo -e "2. Desktop"
+  echo -e "3. Undercover"
+  echo -e "4. Reverse Undercover"
+  echo -e "5. Exit"
+  read -r -p "Enter the option number: " option
+
+  case $option in
+    1)
+      tools
       ;;
-
-    [nN][oO]|[nN])
+    2)
+      desktop
+      ;;
+    3)
+      undercover
+      ;;
+    4)
+      reverse_undercover
+      ;;
+    5)
       echo -e "${red}Operation canceled.${reset}"
-      break
       exit 0
       ;;
-
     *)
-      echo -e "${red}Invalid input...${reset}"
+      echo -e "${red}Invalid option. Please try again.${reset}"
       ;;
-
   esac
 done
-
-# sourcing
-if [ "$INSTALL" = true ]
-  then
-  cd $HOME_DIR
-  apt-get update
-
-  if [ "$DESKTOP" = true ]
-    then
-    chmod +x $DESKTOP_DIR
-    exec $DESKTOP_DIR
-  fi
-
-  if [ "$TOOLS" = true ]
-    then
-    chmod +x $TOOL_DIR
-    exec $TOOL_DIR
-  fi
-fi
